dcd9509bdd791200dc53302474bd126381fd664f
deniznal <deniz.nal@ozu.edu.tr>
2024-11-24 23:22:11 +0300

    updated the recipe class to better fit the API

--- app/src/main/java/com/example/recipeappstep1/model/Recipe.kt
+++ app/src/main/java/com/example/recipeappstep1/model/Recipe.kt
@@ -3,7 +3,7 @@ package com.example.recipeappstep1.model
 import java.io.Serializable
 
 data class Recipe(
-    val name:String, val ingredients: MutableList<Ingredient>, val instructions:String, val category:String, val image:String,
-    val id:Int, val tags: String):
+    val strMeal:String, val ingredients: MutableList<Ingredient>, val strInstructions:String, val strCategory:String, val strMealThumb:String,
+    val idMeal:Int):
     Serializable
 
