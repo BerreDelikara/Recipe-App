1d195729c840f0f91f551c592c32579b4ef75581
BerreDelikara <146889123+BerreDelikara@users.noreply.github.com>
2024-11-26 12:35:31 +0300

    categoryviewmodel added

--- app/src/main/java/com/example/recipeappstep1/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/example/recipeappstep1/viewmodel/CategoryViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.recipeappstep1.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.recipeappstep1.model.Category
+import com.example.recipeappstep1.model.Recipe
+import com.example.recipeappstep1.network.ApiCall
 
 class CategoryViewModel:ViewModel() {
+    private val _categories = MutableLiveData<List<Category>>()
+    val categories: LiveData<List<Category>> get() = _categories
 
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories() {
+        try {
+            val categoryList = ApiCall().getCategories()
+            _categories.postValue(categoryList)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            _categories.postValue(emptyList())
+        }
+    }
 }
